ea869d4b5b887a82e4c1126b4502d18e73a8110d
gudipudipradeep <deepu.rao90@gmail.com>
2021-05-09 08:37:46 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "databricks_host" {
-  value = "https://${azurerm_databricks_workspace.this.workspace_url}/"
+  value = "https://${azurerm_databricks_workspace.wrkspace.workspace_url}/"
 }
 
 output "vm_password" {
